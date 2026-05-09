44dfd882cac4f1fe5cfa43ec78775c8cc344fe46
SkyFooJS <skyfoojs@gmail.com>
2026-05-09 15:07:54 +0800

    feat: add sendWebchatMessageAPI

--- src/features/chat/api/chat.ts
+++ src/features/chat/api/chat.ts
@@ -37,6 +37,34 @@ export async function uploadChatFile(file: File): Promise<string> {
   return finalUrl;
 }
 
+export const sendWebchatMessageAPI = async (
+  token: string,
+  mobileNo: string,
+  message?: string,
+  mediaUrl?: string,
+  mediaName?: string,
+): Promise<Response | undefined> => {
+  try {
+    return await fetch(`${backendServerConfig.backendServerUrl}/chat/webchat`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        mobile_no: mobileNo,
+        message,
+        media_url: mediaUrl,
+        media_name: mediaName,
+        created_by: 'WEBCHAT',
+      }),
+      mode: 'cors',
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export const getChatHistoryAPI = async (token: string, mobileNo: string): Promise<Response | undefined> => {
   try {
     return await fetch(`${backendServerConfig.backendServerUrl}/webchat/history/${mobileNo}`, {
